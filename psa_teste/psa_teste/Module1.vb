1f11972e331b1f9e17505259be801908bd826ea1
nunoseabra <145006257+nunoseabra@users.noreply.github.com>
2024-03-12 09:59:03 +0000

    Update Module1.vb

--- psa_teste/psa_teste/Module1.vb
+++ psa_teste/psa_teste/Module1.vb
@@ -2,7 +2,7 @@
 
 Module Module1
 
-    Public credenciaisValidas() As String = {"0307546191", "0307598534"}
+    Public credenciaisValidas() As String = {"0307546191", "0307598534","0858993457"}
     Public credencial As String
     Public _credencial As String
 
